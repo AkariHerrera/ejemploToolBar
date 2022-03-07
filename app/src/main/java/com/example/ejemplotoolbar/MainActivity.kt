13bcecbb86d2e50014e51b7a07ca12f0ff6b46d2
AkariHerrera <akari.herrera@cbtis72.edu.mx>
2022-03-07 06:56:41 -0500

    commit

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.ejemplotoolbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuItemCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar=findViewById(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+
+        var bIr = findViewById<Button>(R.id.bIr)
+
+        bIr.setOnClickListener{
+            val intent = Intent(this,PantallaDos::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val itemBusqueda = menu?.findItem(R.id.busqueda)
+        var vistaBusqueda = itemBusqueda?.actionView as SearchView
+        val itemCompartir = menu?.findItem(R.id.share)
+        val shareActionProvider = MenuItemCompat.getActionProvider(itemCompartir) as ShareActionProvider
+        compartirIntent(shareActionProvider)
+
+        vistaBusqueda.queryHint = "Escribe tu nombre..."
+        vistaBusqueda.setOnQueryTextFocusChangeListener {v,hasFocus ->
+            Log.d("LISTENERFOCUS",hasFocus.toString())
+        }
+        vistaBusqueda.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("OnQueryTextChange",newText.toString())
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("OnQueryTextSubmit",query.toString())
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bfav -> {
+                Toast.makeText(this,"Elemento añadido como favorito", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else ->{
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+    private fun compartirIntent(shareActionProvider: ShareActionProvider){
+        if(shareActionProvider != null){
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type="text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,"Este es un mensaje compartido")
+            shareActionProvider.setShareIntent(intent)
+        }
     }
 }
